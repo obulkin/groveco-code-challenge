367916024497665adf2bf9cac9e3d057cbfbf4e2
Oleg Bulkin <o.bulkin@gmail.com>
2018-03-20 18:23:23 -0700

    Create CLI wrapper

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='find_store',
+    version='1.0.0',
+    packages=['find_store'],
+    entry_points={
+        'console_scripts': [
+            'find_store = find_store.__main__:main',
+        ],
+    },
+)
