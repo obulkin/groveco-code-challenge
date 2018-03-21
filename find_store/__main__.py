71ff77643963d1c5ac40acb32c63f23cc7c021f4
Oleg Bulkin <o.bulkin@gmail.com>
2018-03-21 13:00:59 -0700

    * Add support for converting address/zip into latitude and longitude using Google Maps API

--- find_store/__main__.py
+++ find_store/__main__.py
@@ -1,4 +1,7 @@
 from argparse import ArgumentParser
+from geocoder import google as google_geo
+from requests import RequestException
+import sys
 import pdb
 
 
@@ -8,6 +11,20 @@ def main():
     main_arg_group.add_argument('--address', type=str)
     main_arg_group.add_argument('--zip', type=str)
     args = parser.parse_args()
+
+    # Use Geocoder library (https://github.com/DenisCarriere/geocoder) to
+    # convert address or zip into coordinates via Google Maps API
+    try:
+        address = args.address if args.address else args.zip
+        geo_data = google_geo(address)
+    except RequestException:
+        sys.exit('There was an issue connecting to the Google Maps API. Please'
+                 ' check network status and try again.')
+    location_latlng = geo_data.latlng
+    if not location_latlng:
+        sys.exit('The Google Maps API did not return a result. Please check'
+                 ' your API credentials and the provided address or zip code'
+                 ' before trying again.')
 
     # pdb.set_trace()
     print('Running!')
