6c0d725d5243f76d364734af24f9af0f6ccfa253
Oleg Bulkin <o.bulkin@gmail.com>
2018-03-20 19:10:44 -0700

    * Add logic for parsing main CLI arguments * Create .gitignore file

--- find_store/__main__.py
+++ find_store/__main__.py
@@ -1,4 +1,15 @@
+from argparse import ArgumentParser
+import pdb
+
+
 def main():
+    parser = ArgumentParser()
+    main_arg_group = parser.add_mutually_exclusive_group(required=True)
+    main_arg_group.add_argument('--address', type=str)
+    main_arg_group.add_argument('--zip', type=str)
+    args = parser.parse_args()
+
+    # pdb.set_trace()
     print('Running!')
 
 
