f111228e658586fb2be680f00bb1b4495d1ee44f
Oleg Bulkin <o.bulkin@gmail.com>
2018-03-22 18:44:56 -0700

    * Add support for unit and output style options

--- find_store/__main__.py
+++ find_store/__main__.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 from geocoder import google as google_geo
+import json
 from math import atan2, cos, pi, radians, sin, sqrt
 from os import path
 from requests import RequestException
@@ -7,12 +8,13 @@ import sys
 from unicodecsv import DictReader
 import pdb
 
+EARTH_RADIUS_KM = 6371
 EARTH_RADIUS_MI = 3959
 
 
 def haversine(lat1, lon1, lat2, lon2):
-    """Calculate distance between two coordinates on the surface of the Earth
-    using the haversine formula as described at
+    """Calculate distance between two points on the surface of the Earth using
+    the haversine formula as described at
     https://www.movable-type.co.uk/scripts/latlong.html
     """
     lat1_rad = radians(lat1)
@@ -31,13 +33,21 @@ def main():
     main_arg_group = parser.add_mutually_exclusive_group(required=True)
     main_arg_group.add_argument('--address', type=str)
     main_arg_group.add_argument('--zip', type=str)
+    parser.add_argument('--units', choices=['mi', 'km'])
+    parser.add_argument('--output', choices=['text', 'json'])
     args = parser.parse_args()
+    if args.address:
+        starting_location = args.address
+    elif args.zip:
+        starting_location = args.zip
+    else:
+        sys.exit('You must provide a starting address or zip code to use this'
+                 ' tool. Please try again.')
 
     # Use Geocoder library (https://github.com/DenisCarriere/geocoder) to
     # convert address or zip into coordinates via Google Maps API
     try:
-        address = args.address if args.address else args.zip
-        geo_data = google_geo(address)
+        geo_data = google_geo(starting_location)
     except RequestException:
         sys.exit('There was an issue connecting to the Google Maps API. Please'
                  ' check network status and try again.')
@@ -62,9 +72,27 @@ def main():
                 closest_store = store
                 closest_distance = distance
 
-    print closest_store
-    print closest_distance
-    # pdb.set_trace()
+    if args.units == 'km':
+        closest_distance = closest_distance * EARTH_RADIUS_KM / EARTH_RADIUS_MI
+    else:
+        args.units = 'mi'
+
+    if args.output == 'json':
+        closest_store['Distance'] = closest_distance
+        closest_store['Distance Units'] = args.units
+        closest_store['Latitude'] = float(closest_store['Latitude'])
+        closest_store['Longitude'] = float(closest_store['Longitude'])
+        return json.dumps(closest_store)
+    else:
+        print '%s Location' % closest_store['Store Name']
+        print closest_store['Store Location']
+        print closest_store['Address']
+        print '%s, %s %s' % (
+            closest_store['City'],
+            closest_store['State'],
+            closest_store['Zip Code'],
+        )
+        print '%s %s' % (round(closest_distance, 2), args.units)
 
 
 if __name__ == '__main__':
